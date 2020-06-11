ca6d8445ba98b4674c95eaf7ce365484aae871c3
Michał Sikora <me@mehow.io>
2020-06-11 11:52:49 +0200

    Rename hyperion module to hyperion-plugin module.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 include(":library:laboratory")
 include(":library:android")
 include(":library:inspector")
-include(":library:hyperion")
+include(":library:hyperion-plugin")
 include(":library:generator")
