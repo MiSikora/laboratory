f98cd2f00fcb0d649df813446e2e677997775185
Michał Sikora <michalsikora90@gmail.com>
2020-10-05 21:22:39 +0200

    Make observed features distinctive for in-memory storage

--- library/laboratory/src/main/java/io/mehow/laboratory/FeatureStorage.kt
+++ library/laboratory/src/main/java/io/mehow/laboratory/FeatureStorage.kt
@@ -3,6 +3,7 @@ package io.mehow.laboratory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 
 interface FeatureStorage {
@@ -15,7 +16,9 @@ interface FeatureStorage {
       private var features = emptyMap<Class<*>, String>()
       private val featureFlow = MutableStateFlow(features)
 
-      override fun <T : Feature<*>> observeFeatureName(featureClass: Class<T>) = featureFlow.map { it[featureClass] }
+      override fun <T : Feature<*>> observeFeatureName(featureClass: Class<T>) = featureFlow
+        .map { it[featureClass] }
+        .distinctUntilChanged()
 
       override suspend fun <T : Feature<*>> getFeatureName(featureClass: Class<T>) = features[featureClass]
 
