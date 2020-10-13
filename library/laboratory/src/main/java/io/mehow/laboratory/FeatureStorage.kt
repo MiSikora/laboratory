4effc84dc19be3c5a7d354ac16aac883d16d20d3
Michał Sikora <michalsikora90@gmail.com>
2020-10-13 17:36:39 +0200

    Add blank spaces in interface definition

--- library/laboratory/src/main/java/io/mehow/laboratory/FeatureStorage.kt
+++ library/laboratory/src/main/java/io/mehow/laboratory/FeatureStorage.kt
@@ -4,9 +4,12 @@ import kotlinx.coroutines.flow.Flow
 
 interface FeatureStorage {
   fun <T : Feature<*>> observeFeatureName(featureClass: Class<T>): Flow<String?>
+
   suspend fun <T : Feature<*>> getFeatureName(featureClass: Class<T>): String?
-  @JvmDefault suspend fun <T : Feature<*>> setFeature(feature: T): Boolean = setFeatures(feature)
+
   suspend fun <T : Feature<*>> setFeatures(vararg features: T): Boolean
+
+  @JvmDefault suspend fun <T : Feature<*>> setFeature(feature: T): Boolean = setFeatures(feature)
 
   companion object {
     fun inMemory(): FeatureStorage = InMemoryFeatureStorage()
@@ -17,4 +20,3 @@ interface FeatureStorage {
     ): FeatureStorage = SourcedFeatureStorage(localSource, remoteSources)
   }
 }
-
