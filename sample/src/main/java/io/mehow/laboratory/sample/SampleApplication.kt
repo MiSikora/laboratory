8e49c1ea4213013e1b3cb3c84bc2c4c74f95f626
Michał Sikora <michalsikora90@gmail.com>
2020-10-22 21:29:57 +0200

    Simulate changes of remote feature flag values

--- sample/src/main/java/io/mehow/laboratory/sample/SampleApplication.kt
+++ sample/src/main/java/io/mehow/laboratory/sample/SampleApplication.kt
@@ -1,29 +1,64 @@
 package io.mehow.laboratory.sample
 
 import android.app.Application
+import io.mehow.laboratory.Feature
 import io.mehow.laboratory.FeatureFactory
 import io.mehow.laboratory.FeatureStorage
 import io.mehow.laboratory.Laboratory
+import io.mehow.laboratory.a.Authentication
+import io.mehow.laboratory.b.PowerSource
+import io.mehow.laboratory.c.DistanceAlgorithm
 import io.mehow.laboratory.inspector.LaboratoryActivity
 import io.mehow.laboratory.sharedpreferences.sharedPreferences
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class SampleApplication : Application() {
   lateinit var laboratory: Laboratory
     private set
+  private lateinit var localStorage: FeatureStorage
+  private lateinit var firebaseStorage: FeatureStorage
+  private lateinit var awsStorage: FeatureStorage
+  private lateinit var azureStorage: FeatureStorage
 
   override fun onCreate() {
     super.onCreate()
+    localStorage = FeatureStorage.sharedPreferences(this, "localFeatures")
+    firebaseStorage = FeatureStorage.sharedPreferences(this, "firebaseFeatures")
+    awsStorage = FeatureStorage.sharedPreferences(this, "awsFeatures")
+    azureStorage = FeatureStorage.sharedPreferences(this, "azureStorage")
     val sourcedStorage = FeatureStorage.sourcedGenerated(
-        localSource = FeatureStorage.sharedPreferences(this, "localFeatures"),
-        firebaseSource = FeatureStorage.sharedPreferences(this, "firebaseFeatures"),
-        awsSource = FeatureStorage.sharedPreferences(this, "awsFeatures"),
-        azureSource = FeatureStorage.sharedPreferences(this, "azureStorage"),
+        localSource = localStorage,
+        firebaseSource = firebaseStorage,
+        awsSource = awsStorage,
+        azureSource = azureStorage
     )
     laboratory = Laboratory(sourcedStorage)
     LaboratoryActivity.configure(
         laboratory = laboratory,
         featureFactory = FeatureFactory.featureGenerated(),
     )
+    observeRemoteFeatures()
+  }
+
+  private fun observeRemoteFeatures() {
+    firebaseStorage.observeRemoteFeature<DistanceAlgorithm>()
+    azureStorage.observeRemoteFeature<DistanceAlgorithm>()
+    firebaseStorage.observeRemoteFeature<PowerSource>()
+    awsStorage.observeRemoteFeature<Authentication>()
+  }
+
+  private inline fun <reified T : Feature<T>> FeatureStorage.observeRemoteFeature() = GlobalScope.launch {
+    val laboratory = Laboratory(this@observeRemoteFeature)
+    val featureValues = T::class.java.enumConstants!!
+    while (isActive) {
+      delay(10_000)
+      val nextFeatureValue = featureValues[Random.nextInt(featureValues.size)]
+      laboratory.setFeature(nextFeatureValue)
+    }
   }
 
   companion object {
