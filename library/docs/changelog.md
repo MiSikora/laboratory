e848b63d787b4f8b00c5aac4dd9dc13b0ca52a63
Michał Sikora <michalsikora90@gmail.com>
2020-12-07 17:22:25 +0100

    Synchronize updates to in-memory laboratory

--- library/docs/changelog.md
+++ library/docs/changelog.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Deprecated
 - Using `DeprecationLevel.Hidden` is temporarily treated as an error until the [compiler issue](https://github.com/MiSikora/laboratory/issues/62) is fixed.
 
+### Fixed
+- Make updates to the in-memory `Laboratory` atomic.
+
 ## [0.9.5] - 2020-12-03
 
 ### Added
--- library/laboratory/src/main/java/io/mehow/laboratory/InMemoryFeatureStorage.kt
+++ library/laboratory/src/main/java/io/mehow/laboratory/InMemoryFeatureStorage.kt
@@ -2,29 +2,27 @@ package io.mehow.laboratory
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
 internal class InMemoryFeatureStorage : FeatureStorage {
-  private var features = emptyMap<Class<*>, String>()
-  private val featureFlow = MutableStateFlow(features)
+  private val featureFlow = MutableStateFlow(emptyMap<Class<*>, String>())
 
   override fun <T : Feature<*>> observeFeatureName(feature: Class<T>) = featureFlow
       .map { it[feature] }
       .distinctUntilChanged()
 
-  override suspend fun <T : Feature<*>> getFeatureName(feature: Class<T>) = features[feature]
+  override suspend fun <T : Feature<*>> getFeatureName(feature: Class<T>) = featureFlow.first()[feature]
 
-  override suspend fun clear(): Boolean {
-    features = emptyMap()
+  override suspend fun clear() = synchronized(this) {
     featureFlow.value = emptyMap()
-    return true
+    true
   }
 
-  override suspend fun <T : Feature<*>> setOptions(vararg options: T): Boolean {
-    for (option in options) {
-      features += option.javaClass to option.name
-    }
-    featureFlow.value = features
-    return true
+  override suspend fun <T : Feature<*>> setOptions(vararg options: T) = synchronized(this) {
+    val features = featureFlow.value
+    val newFeatures = options.associate { it.javaClass to it.name }
+    featureFlow.value = features + newFeatures
+    true
   }
 }
