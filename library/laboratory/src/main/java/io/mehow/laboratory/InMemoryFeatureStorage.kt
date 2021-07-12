8eab0d9b4c9944b901260ffe0b79fd90e6f1ee89
Michał Sikora <michalsikora90@gmail.com>
2021-07-12 17:20:37 +0200

    Remove mutex from the in–memory flow

--- library/laboratory/src/main/java/io/mehow/laboratory/InMemoryFeatureStorage.kt
+++ library/laboratory/src/main/java/io/mehow/laboratory/InMemoryFeatureStorage.kt
@@ -4,12 +4,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.flow.update
 
 internal class InMemoryFeatureStorage : FeatureStorage {
   private val featureFlow = MutableStateFlow(emptyMap<Class<*>, String>())
-  private val updateMutex = Mutex()
 
   override fun <T : Feature<*>> observeFeatureName(feature: Class<T>) = featureFlow
       .map { it[feature] }
@@ -18,13 +16,13 @@ internal class InMemoryFeatureStorage : FeatureStorage {
   override suspend fun <T : Feature<*>> getFeatureName(feature: Class<T>) = featureFlow.map { it[feature] }.first()
 
   override suspend fun clear(): Boolean {
-    updateMutex.withLock { featureFlow.value = emptyMap() }
+    featureFlow.update { emptyMap() }
     return true
   }
 
   override suspend fun <T : Feature<*>> setOptions(vararg options: T): Boolean {
     val newFeatures = options.associate { it.javaClass to it.name }
-    updateMutex.withLock { featureFlow.value += newFeatures }
+    featureFlow.update { it + newFeatures }
     return true
   }
 }
