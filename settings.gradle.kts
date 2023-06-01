3224aab1059bf834fa72f4a2787f03a84b9c483c
Artur Kosta <artur.kosta@droidsonroids.pl>
2023-06-01 20:11:28 +0200

    Migrate settings.gradle files to kts

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+include(":samples:basic")
+include(":samples:default-option")
+include(":samples:multi-module")
+include(":samples:multi-module:multi-module-a")
+include(":samples:multi-module:multi-module-b")
+include(":samples:multi-module:multi-module-c")
+include(":samples:firebase")
+include(":samples:supervision")
+include(":samples:ci-check")
+
+includeBuild("library")
