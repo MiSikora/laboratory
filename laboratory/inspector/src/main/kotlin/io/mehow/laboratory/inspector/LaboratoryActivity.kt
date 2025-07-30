540ba8fccd5cd99356cb13d38c2fa55e69ad20f1
Michał Sikora <me@mehow.io>
2025-07-30 20:22:39 +0200

    Fix clipping padding

--- laboratory/inspector/src/main/kotlin/io/mehow/laboratory/inspector/LaboratoryActivity.kt
+++ laboratory/inspector/src/main/kotlin/io/mehow/laboratory/inspector/LaboratoryActivity.kt
@@ -75,7 +75,6 @@ public class LaboratoryActivity : AppCompatActivity(R.layout.io_mehow_laboratory
         view.updatePadding(
           left = padding.left + bars.left,
           right = padding.right + bars.right,
-          bottom = padding.bottom + bars.bottom,
         )
       }
     }
