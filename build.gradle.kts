576c8cd636b8a29decb53e7331cdba2ff62f2e74
Michał Sikora <me@mehow.io>
2026-01-06 22:48:20 +0100

    Migrate to Dokka 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.dokka.gradle.DokkaExtension
+import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
+
 plugins {
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.kotlin.android) apply false
@@ -17,8 +20,34 @@ apiValidation {
   nonPublicMarkers.add("io.mehow.laboratory.internal.InternalLaboratoryApi")
 }
 
-tasks.dokkaHtmlMultiModule {
+dokka {
   moduleName.set("Laboratory")
   moduleVersion.set(project.name)
-  outputDirectory.set(rootDir.resolve("docs/api"))
+  dokkaPublications.html { outputDirectory.set(rootDir.resolve("docs/api")) }
+}
+
+subprojects {
+  val dokkaId = rootProject.libs.plugins.dokka.get().pluginId
+  pluginManager.withPlugin(dokkaId) {
+    configure<DokkaExtension>() {
+      dokkaSourceSets.configureEach {
+        documentedVisibilities.add(VisibilityModifier.Public)
+        reportUndocumented.set(true)
+        perPackageOption {
+          matchingRegex.set(".*\\.internal.*")
+          suppress.set(true)
+        }
+      }
+    }
+  }
+}
+
+dependencies {
+  dokka(project(":laboratory:data-store"))
+  dokka(project(":laboratory:generator"))
+  dokka(project(":laboratory:gradle-plugin"))
+  dokka(project(":laboratory:hyperion-plugin"))
+  dokka(project(":laboratory:inspector"))
+  dokka(project(":laboratory:runtime"))
+  dokka(project(":laboratory:shared-preferences"))
 }
